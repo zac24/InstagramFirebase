bacf084d115a7ba83024528e440ad3e54961fc30
Prashant Dwivedi <user@jios-MacBook-Pro.local>
2018-03-06 18:06:31 +0530

    added assets and created plus button

--- InstagramFirebase/InstagramFirebase/ViewController.swift
+++ InstagramFirebase/InstagramFirebase/ViewController.swift
@@ -9,10 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let plusPhotoButton : UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false;
+        button.setImage(#imageLiteral(resourceName: "plus_photo"), for: .normal)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        view.addSubview(plusPhotoButton)
+        
+        plusPhotoButton.widthAnchor.constraint(equalToConstant: 140).isActive = true
+        plusPhotoButton.heightAnchor.constraint(equalToConstant: 140).isActive = true
+        plusPhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        plusPhotoButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 40).isActive = true
     }
 
     override func didReceiveMemoryWarning() {
