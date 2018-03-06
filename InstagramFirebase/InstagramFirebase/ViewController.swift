814d414c2b6b4e73e51e3109253e83c77533c822
Prashant Dwivedi <user@jios-MacBook-Pro.local>
2018-03-06 17:13:13 +0530

    first commit of empty project

---
+++ InstagramFirebase/InstagramFirebase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstagramFirebase
+//
+//  Created by Prashant Dwivedi on 3/6/18.
+//  Copyright © 2018 Prashant Dwivedi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
