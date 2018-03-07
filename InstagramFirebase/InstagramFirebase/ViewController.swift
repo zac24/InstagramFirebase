2ce08bf4a4311652e41a8d96daf6bb14bc6cd86f
Prashant Dwivedi <user@jios-MacBook-Pro.local>
2018-03-07 13:36:34 +0530

    emailTextField created

--- InstagramFirebase/InstagramFirebase/ViewController.swift
+++ InstagramFirebase/InstagramFirebase/ViewController.swift
@@ -16,17 +16,38 @@ class ViewController: UIViewController {
         button.setImage(#imageLiteral(resourceName: "plus_photo"), for: .normal)
         return button
     }()
+    
+    let emailTextField : UITextField = {
+       
+        let tf = UITextField()
+        tf.placeholder = "Email"
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.borderStyle = .roundedRect
+        tf.font = UIFont.systemFont(ofSize: 14)
+        return tf
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         view.addSubview(plusPhotoButton)
+        view.addSubview(emailTextField)
         
         plusPhotoButton.widthAnchor.constraint(equalToConstant: 140).isActive = true
         plusPhotoButton.heightAnchor.constraint(equalToConstant: 140).isActive = true
         plusPhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         plusPhotoButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 40).isActive = true
+        
+        NSLayoutConstraint.activate(
+            [emailTextField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40),
+            emailTextField.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -40),
+            emailTextField.topAnchor.constraint(equalTo: plusPhotoButton.bottomAnchor, constant: 20),
+            emailTextField.heightAnchor.constraint(equalToConstant: 50)]
+        )
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
