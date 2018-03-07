1a2fe5f76b64a98a94a91735c9b48e9e2a546a9c
Prashant Dwivedi <user@jios-MacBook-Pro.local>
2018-03-07 16:07:04 +0530

    created rest of the field

--- InstagramFirebase/InstagramFirebase/ViewController.swift
+++ InstagramFirebase/InstagramFirebase/ViewController.swift
@@ -18,7 +18,6 @@ class ViewController: UIViewController {
     }()
     
     let emailTextField : UITextField = {
-       
         let tf = UITextField()
         tf.placeholder = "Email"
         tf.translatesAutoresizingMaskIntoConstraints = false
@@ -27,34 +26,78 @@ class ViewController: UIViewController {
         tf.font = UIFont.systemFont(ofSize: 14)
         return tf
     }()
+    let usernameTextField : UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Username"
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.borderStyle = .roundedRect
+        tf.font = UIFont.systemFont(ofSize: 14)
+        return tf
+    }()
+    let passwordTextField : UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Password"
+        tf.isSecureTextEntry = true
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.borderStyle = .roundedRect
+        tf.font = UIFont.systemFont(ofSize: 14)
+        return tf
+    }()
+    
+    let signupButton : UIButton = {
+        let button = UIButton(type:.system)
+        button.setTitle("Sign Up", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.backgroundColor = UIColor(red: 149/255, green: 204/255, blue: 244/255, alpha: 1)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 5
+        button.setTitleColor(.white, for: .normal)
+        
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         view.addSubview(plusPhotoButton)
-        view.addSubview(emailTextField)
+        
         
         plusPhotoButton.widthAnchor.constraint(equalToConstant: 140).isActive = true
         plusPhotoButton.heightAnchor.constraint(equalToConstant: 140).isActive = true
         plusPhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         plusPhotoButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 40).isActive = true
         
+        
+        setupInputFields ()
+        
+        
+        
+    }
+    
+    fileprivate func setupInputFields() {
+        
+        let stackView = UIStackView(arrangedSubviews: [emailTextField, usernameTextField, passwordTextField, signupButton])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.distribution = .fillEqually
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        
+         view.addSubview(stackView)
+        
         NSLayoutConstraint.activate(
-            [emailTextField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40),
-            emailTextField.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -40),
-            emailTextField.topAnchor.constraint(equalTo: plusPhotoButton.bottomAnchor, constant: 20),
-            emailTextField.heightAnchor.constraint(equalToConstant: 50)]
+            [stackView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40),
+             stackView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -40),
+             stackView.topAnchor.constraint(equalTo: plusPhotoButton.bottomAnchor, constant: 20),
+             stackView.heightAnchor.constraint(equalToConstant: 230)]
         )
         
+       
+        
+        
         
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
